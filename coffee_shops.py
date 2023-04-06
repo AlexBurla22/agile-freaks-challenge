9ae763cdd2e55d31d3e9faf66619daa714f5e0c6
Burla Alexandru <alex.burla@rocketmail.com>
2023-04-06 14:36:25 +0300

    Implemented get_data() function

--- coffee_shops.py
+++ coffee_shops.py
@@ -4,6 +4,9 @@ import pandas as pd
 def get_arguments(argv):
     if(len(argv) != 3):
         print('Please provide input as <user x coordinate> <user y coordinate> <shop data url>')
+        x_coord = 0
+        y_coord = 0
+        url = ''
     else:
         try:
             x_coord = float(argv[0])
@@ -13,8 +16,14 @@ def get_arguments(argv):
         url = argv[2]
     return x_coord, y_coord, url
 
+def get_data(url):
+    return pd.read_csv(url)
+
 def main(argv):
     x, y, url = get_arguments(argv)
+    if(url != ''):
+        shops = get_data(url)
+        print(shops)
     input('Press enter to exit...')
 
 if __name__ == "__main__":
