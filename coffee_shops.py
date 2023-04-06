0c4b24128a809f8d2c195fa3e2727d7fe81807a4
Burla Alexandru <alex.burla@rocketmail.com>
2023-04-06 15:29:06 +0300

    Implemented functions for calculating distances

--- coffee_shops.py
+++ coffee_shops.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import math
 
 def get_arguments(argv):
     if(len(argv) != 3):
@@ -17,14 +18,21 @@ def get_arguments(argv):
     return x_coord, y_coord, url
 
 def get_data(url):
-    return pd.read_csv(url)
+    return pd.read_csv(url, names=['name', 'x_coord', 'y_coord'])
+
+def calculate_distances(data, position):
+    return pd.DataFrame(data).apply(calculate_distance, args=(position,), axis=1)
+
+def calculate_distance(row, position):
+    shop_position = float(row['x_coord']), float(row['y_coord'])
+    return math.sqrt(math.pow(shop_position[0] - position[0], 2) + math.pow(shop_position[1] - position[1], 2))
 
 def main(argv):
     x, y, url = get_arguments(argv)
     if(url != ''):
         shops = get_data(url)
-        print(shops)
+        list_distances = calculate_distances(shops, (x, y))
     input('Press enter to exit...')
-
+ 
 if __name__ == "__main__":
     main(sys.argv[1:])
